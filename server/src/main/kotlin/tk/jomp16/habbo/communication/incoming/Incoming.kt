c043a8893d72a2194bbce693a8506050ad8fb8fe
José Olívio M. Pedrosa <joseoliviopedrosa@gmail.com>
2016-08-04 11:42:10 -0300

    Remove ping

--- server/src/main/kotlin/tk/jomp16/habbo/communication/incoming/Incoming.kt
+++ server/src/main/kotlin/tk/jomp16/habbo/communication/incoming/Incoming.kt
@@ -99,5 +99,4 @@ object Incoming {
     const val NAVIGATOR_CREATE_ROOM = 1822 // CreateFlatMessageEvent
     const val CATALOG_PURCHASE = 730 // PurchaseFromCatalogMessageEvent
     const val ROOM_USER_DANCE = 3688 // DanceMessageEvent
-    const val PING = 1006 // PingMessageEvent
 }
