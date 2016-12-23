2a04595fa6cef291b04049a1a9114e4729503c20
José Olívio M. Pedrosa <joseoliviopedrosa@gmail.com>
2016-12-23 12:27:13 -0300

    Remove disconnecting of non handshake user on HabboGame.
    It may disconnect users in progress of handshaking.

--- server/src/main/kotlin/tk/jomp16/habbo/game/HabboGame.kt
+++ server/src/main/kotlin/tk/jomp16/habbo/game/HabboGame.kt
@@ -21,8 +21,6 @@ package tk.jomp16.habbo.game
 
 import org.jasypt.util.password.PasswordEncryptor
 import org.jasypt.util.password.StrongPasswordEncryptor
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import tk.jomp16.habbo.HabboServer
 import tk.jomp16.habbo.game.catalog.CatalogManager
 import tk.jomp16.habbo.game.item.ItemManager
@@ -36,8 +34,6 @@ import tk.jomp16.habbo.game.user.HabboSession
 import java.util.concurrent.TimeUnit
 
 class HabboGame {
-    private val log: Logger = LoggerFactory.getLogger(javaClass)
-
     val passwordEncryptor: PasswordEncryptor = StrongPasswordEncryptor()
 
     val landingManager: LandingManager
@@ -58,12 +54,6 @@ class HabboGame {
         moderationManager = ModerationManager()
 
         HabboServer.serverScheduledExecutor.scheduleWithFixedDelay({
-            HabboServer.habboSessionManager.habboSessions.values.filter { !it.authenticated }.forEach {
-                log.error("Found an connected user without doing the handshake! Disconnecting it!")
-
-                it.channel.close()
-            }
-
             HabboServer.habboSessionManager.habboSessions.values.filter { it.authenticated && !it.habboSubscription.validUserSubscription }
                     .forEach { it.habboSubscription.clearSubscription() }
         }, 0, 1, TimeUnit.MINUTES)
