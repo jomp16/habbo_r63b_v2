b1c73ef1c0298441650590fc006fc3e7f087a972
José Olívio M. Pedrosa <joseoliviopedrosa@gmail.com>
2017-01-07 15:29:18 -0300

    Fixed #12, now it parses and renders correctly!

--- habbo_imaging/src/main/kotlin/tk/jomp16/habbo/imaging/GroupBadge.kt
+++ habbo_imaging/src/main/kotlin/tk/jomp16/habbo/imaging/GroupBadge.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 jomp16
+ * Copyright (C) 2015-2017 jomp16
  *
  * This file is part of habbo_r63b_v2.
  *
@@ -19,12 +19,13 @@
 
 package tk.jomp16.habbo.imaging
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.awt.Color
 import java.awt.RenderingHints
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
-
 
 class GroupBadge(
         private val badgeBases: Collection<Triple<Int, String, String>>,
@@ -32,6 +33,8 @@ class GroupBadge(
         private val badgeSymbols: Collection<Triple<Int, String, String>>,
         private val badgeSymbolColors: Collection<Pair<Int, String>>
 ) {
+    private val log: Logger = LoggerFactory.getLogger(javaClass)
+
     companion object {
         const val WIDTH = 39
         const val HEIGHT = 39
@@ -45,70 +48,75 @@ class GroupBadge(
         graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
         graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC)
 
-        val split = badgeCode.split('s')
-        val baseCode = split[0].substring(1)
-        val symbolsCode = split.drop(1).filter { it.length >= 4 && it.length <= 6 }
+        try {
+            val split = badgeCode.split('s')
+            val baseCode = split[0].substring(1)
+            val symbolsCode = split.drop(1)
 
-        if (baseCode.length >= 4 && baseCode.length <= 5) {
-            val data = getParts(baseCode, false)
-            val baseData = badgeBases.find { it.first == data[0].toInt() }!!
-            val image = ImageIO.read(javaClass.classLoader.getResourceAsStream("base/badgepart_${File(baseData.second).nameWithoutExtension}.png"))
+            if (baseCode.isNotEmpty()) {
+                val data = getParts(baseCode, false)
+                val baseData = badgeBases.find { it.first == data[0].toInt() }!!
+                val image = ImageIO.read(javaClass.classLoader.getResourceAsStream("base/badgepart_${File(baseData.second).nameWithoutExtension}.png"))
 
-            if (data[1].isNotEmpty()) colorize(image, Color.decode("#${data[1]}"))
-            if (baseData.third.isNotEmpty()) {
-                val image1 = ImageIO.read(javaClass.classLoader.getResourceAsStream("base/badgepart_${File(baseData.third).nameWithoutExtension}.png"))
+                if (data[1].isNotEmpty()) colorize(image, Color.decode("#${data[1]}"))
 
-                graphics.drawImage(image1, 0, 0, null)
-            }
+                graphics.drawImage(image, badgeImage.width / 2 - image.width / 2, badgeImage.height / 2 - image.height / 2, null)
 
-            graphics.drawImage(image, badgeImage.width / 2 - image.width / 2, badgeImage.height / 2 - image.height / 2, null)
-        }
+                if (baseData.third.isNotEmpty()) {
+                    val image1 = ImageIO.read(javaClass.classLoader.getResourceAsStream("base/badgepart_${File(baseData.third).nameWithoutExtension}.png"))
 
-        symbolsCode.forEach {
-            val data = getParts(it, true)
-
-            if (data[0].toInt() == 0) return@forEach
-
-            val symbolData = badgeSymbols.find { it.first == data[0].toInt() }!!
-            val image = ImageIO.read(javaClass.classLoader.getResourceAsStream("symbol/badgepart_${File(symbolData.second).nameWithoutExtension}.png"))
-
-            if (data[1].isNotEmpty()) colorize(image, Color.decode("#${data[1]}"))
-
-            if (symbolData.third.isNotEmpty()) {
-                val image1 = ImageIO.read(javaClass.classLoader.getResourceAsStream("symbol/badgepart_${File(symbolData.third).nameWithoutExtension}.png"))
-
-                graphics.drawImage(image1, 0, 0, null)
-            }
-
-            val position = if (data[2].isNotEmpty()) data[2].toInt() else 0
-
-            var x = 0
-            var y = 0
-
-            when (position) {
-                1 -> x = (badgeImage.width - image.width) / 2
-                2 -> x = badgeImage.width - image.width
-                3 -> y = (badgeImage.height / 2) - (image.height / 2)
-                4 -> {
-                    x = (badgeImage.width / 2) - (image.width / 2)
-                    y = (badgeImage.height / 2) - (image.height / 2)
-                }
-                5 -> {
-                    x = badgeImage.width - image.width
-                    y = (badgeImage.height / 2) - (image.height / 2)
-                }
-                6 -> y = badgeImage.height - image.height
-                7 -> {
-                    x = (badgeImage.width - image.width) / 2
-                    y = badgeImage.height - image.height
-                }
-                8 -> {
-                    x = badgeImage.width - image.width
-                    y = badgeImage.height - image.height
+                    graphics.drawImage(image1, badgeImage.width / 2 - image.width / 2, badgeImage.height / 2 - image.height / 2, null)
                 }
             }
 
-            graphics.drawImage(image, x, y, null)
+            symbolsCode.forEach {
+                val data = getParts(it, true)
+
+                if (data[0].toInt() == 0) return@forEach
+
+                val symbolData = badgeSymbols.find { it.first == data[0].toInt() }!!
+                val image = ImageIO.read(javaClass.classLoader.getResourceAsStream("symbol/badgepart_${File(symbolData.second).nameWithoutExtension}.png"))
+
+                if (data[1].isNotEmpty()) colorize(image, Color.decode("#${data[1]}"))
+
+                val position = if (data[2].isNotEmpty()) data[2].toInt() else 0
+
+                var x = 0
+                var y = 0
+
+                when (position) {
+                    1 -> x = (badgeImage.width - image.width) / 2
+                    2 -> x = badgeImage.width - image.width
+                    3 -> y = (badgeImage.height / 2) - (image.height / 2)
+                    4 -> {
+                        x = (badgeImage.width / 2) - (image.width / 2)
+                        y = (badgeImage.height / 2) - (image.height / 2)
+                    }
+                    5 -> {
+                        x = badgeImage.width - image.width
+                        y = (badgeImage.height / 2) - (image.height / 2)
+                    }
+                    6 -> y = badgeImage.height - image.height
+                    7 -> {
+                        x = (badgeImage.width - image.width) / 2
+                        y = badgeImage.height - image.height
+                    }
+                    8 -> {
+                        x = badgeImage.width - image.width
+                        y = badgeImage.height - image.height
+                    }
+                }
+
+                graphics.drawImage(image, x, y, null)
+
+                if (symbolData.third.isNotEmpty()) {
+                    val image1 = ImageIO.read(javaClass.classLoader.getResourceAsStream("symbol/badgepart_${File(symbolData.third).nameWithoutExtension}.png"))
+
+                    graphics.drawImage(image1, x, y, null)
+                }
+            }
+        } catch (e: Exception) {
+            log.error("An exception happened while rendering group badge $badgeCode", e)
         }
 
         return badgeImage
@@ -127,11 +135,17 @@ class GroupBadge(
     }
 
     private fun getParts(code: String, isSymbol: Boolean): List<String> {
-        val partKey = code.substring(0, if (code.length == 6 || !isSymbol && code.length == 5) 3 else 2)
-        val partColor = code.substring(partKey.length, partKey.length + 2)
-        val partPos = if (code.length > partKey.length + partColor.length) code.substring(partKey.length + partColor.length, code.length) else "0"
+        val tmp = if (isSymbol) badgeSymbols else badgeBases
+
+        val partKey = StringBuilder()
+
+        if (code.startsWith('0')) partKey.append('0').append(tmp.map { it.first }.filter { it < 10 }.find { code.startsWith("0$it") } ?: "0")
+        else partKey.append(tmp.map { it.first }.filter { it > 10 }.find { code.startsWith("$it") } ?: "0")
+
+        val partColor = code.substring(partKey.length, if (isSymbol) code.length - 1 else code.length)
+        val partPos = if (isSymbol) code.substring(code.length - 1) else "0"
         val color = (if (isSymbol) badgeSymbolColors.find { it.first == partColor.toInt() }?.second else badgeBaseColors.find { it.first == partColor.toInt() }?.second) ?: ""
 
-        return listOf(partKey, color, partPos)
+        return listOf(partKey.toString(), color, partPos)
     }
 }
