23f0f25906c163db5e0595a0f18369651f241879
José Olívio M. Pedrosa <root@rwx.ovh>
2018-01-29 15:05:46 -0300

    Bug fix analytics IPs.
    Forgot to update SQL, whoops!
    
    Signed-off-by: José Olívio M. Pedrosa <root@rwx.ovh>

--- server/src/main/resources/sql/users/ips/insert_ip.sql
+++ server/src/main/resources/sql/users/ips/insert_ip.sql
@@ -1,9 +1,23 @@
 INSERT INTO
   `users_ips` (
     `user_id`,
-    `ip`
+    `ip`,
+    `country_code`,
+    `country`,
+    `region_code`,
+    `region`,
+    `timezone`,
+    `latitude`,
+    `longitude`
   )
 VALUES (
   :user_id,
-  :ip
+  :ip,
+  :country_code,
+  :country,
+  :region_code,
+  :region,
+  :timezone,
+  :latitude,
+  :longitude
 )
